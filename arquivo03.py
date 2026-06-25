a069cfb72d38c4974e663c7f2a6f4ee0d3e2d3fe
kowalsk <>
2026-06-25 20:33:13 -0300

    Create arquivo03.py

---
+++ arquivo03.py
@@ -0,0 +1,7 @@
+menu = ("Usuários", "Clientes", "Fornecedores", "Relatórios")
+
+for opcao in menu:
+    print("+", 18 * "-", "+")
+    print("|", opcao.center(18), "|")
+
+print("+", 18 * "-", "+")
