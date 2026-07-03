3291a9e5b485b64662638351e590d2ef55a37b9b
Gabriel062194 <gr913177@gmail.com>
2026-07-03 20:25:31 -0300

    Update jogodavelha.py

--- jogodavelha.py
+++ jogodavelha.py
@@ -7,3 +7,18 @@ def mostrar_tabuleiro():
         if i < 2:
             print("_" * 9)
         print()
+
+def jogar():
+    jogador_atual = "X"
+    jogadas = 0
+
+    while True:
+        mostrar_tabuleiro()
+
+        print(f"Jogador {jogador_atual}")
+        linha = int(input("Escolha a linha (0, 1, 2): "))
+        coluna = int(input("Escolha a coluna (0, 1, 2): "))
+
+        if tabuleiro[linha][coluna] != " ":
+            print("Posição já ocupada! Tente novamente.")
+            continue
