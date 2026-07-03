b2b09350e5b01037fa6f2abd2f01f56b8ed6440e
Gabriel062194 <gr913177@gmail.com>
2026-07-03 19:27:56 -0300

    Update jogodavelha.py

---
+++ jogodavelha.py
@@ -0,0 +1,9 @@
+tabuleiro = [[" " for _ in range(3)] for _ in range(3)]
+
+def mostrar_tabuleiro():
+    print("\nTabuleiro:\n\n")
+    for i in range(3):
+        print(" | ".join(tabuleiro[i]))
+        if i < 2:
+            print("_" * 9)
+        print()
