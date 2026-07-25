aaa52179608b6be677e78b6d3f6ae1775114f371
Gabriel062194 <gr913177@gmail.com>
2026-07-24 21:51:40 -0300

    tatata

--- 0724_TKINTER/labelimagens.py
+++ 0724_TKINTER/labelimagens.py
@@ -5,6 +5,9 @@ root = tk.Tk()
 root.title("SENAI - Sistemas")
 root.geometry("800x600")
 
-# função
+minha_imagem = tk.PhotoImage(file="imagens em hd 4k.jfif")
+
+label = tk.Label(root, image=minha_imagem)
+label.pack(expand=True)
 
 root.mainloop()
