a8e314bf1ab3d051ec7a5498463a54d9f134da00
Gabriel062194 <gr913177@gmail.com>
2026-07-24 21:14:16 -0300

    bibibi

--- widgetbutton.py
+++ widgetbutton.py
@@ -9,12 +9,21 @@ def button_command():
         "Informação",
         "Você clicou no botão!"
     )
-
 button = tk.Button(
     root,
     text="Clique aqui",
-    command=button_command
-)
-button.pack()
+    command=button_command)  
 
+button.pack()
+def button2_command():
+    messagebox.showinfo(
+        "Aviso!",
+        "Você clicou no botão 2!"
+    )
+button2 = tk.Button(
+    root,
+    text="Clique aqui 2",
+    command=button2_command
+)
+button2.pack()
 root.mainloop()
