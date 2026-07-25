ec1f15e1a905c156cfb7281a062bb7dd74446739
Gabriel062194 <gr913177@gmail.com>
2026-07-24 22:14:38 -0300

    compiuter

--- 0724_TKINTER/label.py
+++ 0724_TKINTER/label.py
@@ -5,21 +5,21 @@ root = tk.Tk()
 root.title("SENAI - Sistemas")
 root.geometry("800x600")
 
-label_1 = tk.Label(root, text="Olá")
-label_1.pack()
+# label_1 = tk.Label(root, text="Olá")
+# label_1.pack()
 
-label_2 = tk.Label(root)
-label_2.pack()
-label_2.config(text="Definido depois")
+# label_2 = tk.Label(root)
+# label_2.pack()
+# label_2.config(text="Definido depois")
 
-label_3 = tk.Label(root,
-      text="Olá!",
-      font=("Helvetica", 30))
-label_3.pack(expand=True)
+# label_3 = tk.Label(root,
+#       text="Olá!",
+#       font=("Helvetica", 30))
+# label_3.pack(expand=True)
 
-minha_imagem = tk.PhotoImage(file="minha-imagem.png")
+minha_imagem = tk.PhotoImage(file="python.png")
 
-label = tk.Label(root, image=minha-imagem.png)
+label = tk.Label(root, image=minha_imagem)
 label.pack(expand=True)
 
 root.mainloop()
